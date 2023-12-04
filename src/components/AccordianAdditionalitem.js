dccd9af3a7f57885f8f7f140f68d35df626d10f5
RealSlimShadyy <122858749+RealSlimShadyy@users.noreply.github.com>
2023-12-04 17:49:38 +0530

    added arrows on the accordion

--- src/components/AccordianAdditionalitem.js
+++ src/components/AccordianAdditionalitem.js
@@ -12,8 +12,10 @@ const AddItem = ({title,data}) => {
     return ( 
     
         <div  className="border rounded mb-4 flex flex-col w-full justify-center">
-            <div className="p-4 cursor-pointer w-full flex flex-col w-full justify-center font-brick" style={{ backgroundColor: '#001219' }} onClick={toggleAccordion}>
+            <div className="p-4 cursor-pointer w-full flex w-full justify-between font-brick" style={{ backgroundColor: '#001219' }} onClick={toggleAccordion}>
             <strong><h2 className="text-lg font-semibold" style={{color: '#f2f2f2'}}>{title}</h2></strong>
+            {isOpen && <span class="fa fa-chevron-up fa-xxs " style={{color: '#f2f2f2'}}></span>}
+            {(!isOpen) && <span class="fa fa-chevron-down fa-xxs " style={{color: '#f2f2f2'}}></span>}
             </div>
             {isOpen && <div className='font-brick p-4'>
                 <ol>
--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -4,6 +4,7 @@ import Table from './table';
 
 const AccordionItem = ({ title, data }) => {
   const [isOpen, setIsOpen] = useState(false);
+  
 
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
@@ -11,12 +12,13 @@ const AccordionItem = ({ title, data }) => {
 
   return (
     <div className="border rounded mb-4 flex flex-col w-full justify-center">
-      <div className="p-4 cursor-pointer w-full flex flex-col w-full justify-center font-brick" style={{ backgroundColor: '#001219' }} onClick={toggleAccordion}>
-        <strong><h2 className="text-lg font-semibold" style={{color: '#f2f2f2'}}>{title}</h2></strong>
+      <div className="p-4 cursor-pointer w-full flex justify-between font-brick" style={{ backgroundColor: '#001219' }} onClick={toggleAccordion}>
+      <strong><h2 className="text-lg font-semibold" style={{color: '#f2f2f2'}}>{title}</h2></strong>
+        {isOpen && <span class="fa fa-chevron-up fa-xxs " style={{color: '#f2f2f2'}}></span>}
+        {(!isOpen) && <span class="fa fa-chevron-down fa-xxs " style={{color: '#f2f2f2'}}></span>}
       </div>
       {isOpen && <div className="p-4 font-brick"><Table data={data} /></div>}
     </div> 
-
   );
 };
 
